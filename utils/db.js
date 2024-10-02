38d19fcb31f00aabbe6918172de7daed23e4bc3c
Method Mkoma <gershommethod02@gmail.com>
2024-10-02 13:53:42 +0300

    task 1

--- utils/db.js
+++ utils/db.js
@@ -5,9 +5,9 @@ class DBClient {
     const host = process.env.DB_HOST || 'localhost';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
-    
+
     const uri = `mongodb://${host}:${port}`;
-    
+
     this.client = new MongoClient(uri, { useUnifiedTopology: true });
     this.db = null;
 
